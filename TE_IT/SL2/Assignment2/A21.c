c4ad5dfdd2f5fd075e77110fcd17f7def5429f1d
ImSahilShaikh <43376638+ImSahilShaikh@users.noreply.github.com>
2019-07-29 14:58:27 +0530

    Update A21.c

--- TE_IT/SL2/Assignment2/A21.c
+++ TE_IT/SL2/Assignment2/A21.c
@@ -1,12 +1,10 @@
-//Including All Header Files
-
+//Header files
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <sys/wait.h>
 
-
-//Function to Swap two values
+//Function for swapping
 int swap(int *a,int *b){
 
 		int temp = *a;
@@ -15,13 +13,13 @@ int swap(int *a,int *b){
 }
 
 //Function for bubble sort
-void bubble_sort(int *num,int n){
-
+void bubble_sort(int *num,int n)
+{
 	int i=0,j=0;
-	for(i=0;i<n-1;i++){
-
-		for(j=0;j<n-i-1;j++){
-
+	for(i=0;i<n-1;i++)
+	{
+		for(j=0;j<n-i-1;j++)
+		{
 			if(num[j] > num[j+1])
 			{
 					swap(num+j,num+j+1);
@@ -30,19 +28,12 @@ void bubble_sort(int *num,int n){
 	}
 }
 
-//Defining Main Function
-int main(int argc,char *argv[]){
+//Main function 
+int main(int argc,char *argv[])
+{
 	//Declaring Variables
-	int *num;
-	int pid;
-	int n = 0;
-	int i = 0;
+	int *num,pid,n = 0,i = 0;
 	char *rst,*array[10];
-
-
-
-	printf("\n[INFO] Main Process Id: %d",getpid());
-	printf("\n[INFO] Taking User Input");
 	
 	//Taking Total Number Of Elements From User	
 	printf("\n\nEnter The Number Of Elements: ");
@@ -58,39 +49,37 @@ int main(int argc,char *argv[]){
 		scanf("%d",num+i);
 	}
 
-	
-	printf("\n[INFO] Parent Is Sorting The Data");
+	printf("\nINFO: Parent Is Sorting The Data");
 
 	//Calling Bubble Sort Function
 	bubble_sort(num,n);
-	printf("\n[INFO] Parent Waiting For Child Process Completion");
+	printf("\nINFO: Parent Waiting For Child Process Completion");
 
-		//Creating New Child Process
-		pid=fork();
+	//Creating New Child Process
+	pid=fork();
 		
-		//Assigning Task to Child	
-		if(pid == 0){
-
-					printf("\n[INFO] Child Process Id: %d",getpid());
-					printf("\n[INFO] Childs Parent Process Id: %d",getppid());	
+	//Assigning Task to Child	
+	if(pid == 0)
+	{
+		printf("\nINFO: Child Process Id: %d",getpid());
+		printf("\nINFO: Childs Parent Process Id: %d",getppid());					
 				
-				
-				//Converting Integer To String
-				printf("\n[INFO] Child Converting Integer Values to Strings");
-			for(i = 0;i<n;i++){
-					rst = (char *)malloc(sizeof(int));
-					sprintf(rst,"%d",num[i]);
-					array[i] = (char *)malloc(sizeof(int));
-					array[i] = rst;
-				}
-				array[i]=NULL;
-		    printf("\n[INFO] Calling EXECV");
-			//Calling Execv()	
-			execvp("./exec2",array);
+		//Converting Integer To String
+		for(i = 0;i<n;i++)
+		{
+			rst = (char *)malloc(sizeof(int));
+			sprintf(rst,"%d",num[i]);
+			array[i] = (char *)malloc(sizeof(int));
+			array[i] = rst;
 		}
-		//Remaining Parent Code Will Go HERE	
-		else{
-			printf("\n[INFO] Parent Process Id in pid > 0: %d",getpid());
-			wait(NULL);
-		}
+		array[i]=NULL;
+	    printf("\n Calling execv");
+		execv("./p2",array);
+	}
+	//Remaining code from parent	
+	else
+	{
+		printf("\nINFO: Parent Process Id in pid (greater than 0): %d",getpid());
+		wait(NULL);
+	}
 }
