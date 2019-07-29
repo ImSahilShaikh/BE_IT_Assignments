c1efd720b7028bfa5c27d0ef735661898a7eba0c
ImSahilShaikh <43376638+ImSahilShaikh@users.noreply.github.com>
2019-07-29 14:58:44 +0530

    Update A22.c

--- TE_IT/SL2/Assignment2/A22.c
+++ TE_IT/SL2/Assignment2/A22.c
@@ -1,55 +1,51 @@
-
+//Header files 
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
 
-
-//Defining Function For Binary Search
+//Function For Binary Search
 void binary_search(int num[10],int first,int last){
 
 			int key=0;
 
-
-			printf("\n[INFO] Taking Input For Searching");
-
-			//Taking Input From User For Number To Be Search
-			printf("\nEnter The Value To Be Searched: ");
+			printf("\nINFO: Enter The Value To Be Searched: ");
 			scanf("%d",&key);
 			int mid = 0;
 			
-			//Looping While First And Last Value is not same
-			while(first <= last){
-				 mid = (first + last) / 2 ;
-				if(key < num[mid]){
+			while(first <= last)
+			{
+				mid = (first + last) / 2 ;
+				if(key < num[mid])
+				{
 					last = mid-1;
 				}
-				else if(key > num[mid]){
+				else if(key > num[mid])
+				{
 					first = mid+1;
-				}else{
-					printf("\nElement Found At %d Location",mid );
+				}
+				else
+				{
+					printf("\nINFO: Element Found At %d Location",mid+1 );
 					return;
 				}
 			}
-
-			printf("\nElement Is Not Found\n");
+			printf("\nINFO: Element not found!!\n");
 }
 
-//Defining Main Function
+//Main Function
 int main(int argc,char *argv[]){
 	
-	//Declaring Variables
+	//Variable Declaration
 	int num[argc-1];
 	int i=0;
 
-
-	printf("\n[INFO] Converting Strings To Integer Values");
+	printf("\nINFO: Converting Strings To Integer Values");
 	//Converting Strings To Array
 	for(i=0;i<argc;i++){
 		num[i] = atoi(argv[i]);
 	}
 
-
-	printf("\n[INFO] Calling Binary Search Function");
+	printf("\nINFO: Calling Binary Search Function");
 	
 	//Calling Function binary_search()
 	binary_search(num,0,argc);
