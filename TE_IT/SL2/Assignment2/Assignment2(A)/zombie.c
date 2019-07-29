dff2d1ea8258ec693fa39230abca2bed1b929844
ImSahilShaikh <43376638+ImSahilShaikh@users.noreply.github.com>
2019-07-29 15:32:59 +0530

    Update zombie.c

--- TE_IT/SL2/Assignment2/Assignment2(A)/zombie.c
+++ TE_IT/SL2/Assignment2/Assignment2(A)/zombie.c
@@ -1,64 +1,87 @@
-//include required libraries
-#include<sys/types.h>
-#include<sys/wait.h>
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
+//header files
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
 
-//function to swap values
-void swap(int *xp, int *yp)
-{
-	  int temp = *xp; 
-    *xp = *yp; 
-    *yp = temp; 
-}
+//function for bubble sort
+void bubble_sort(int num[10],int n){
 
-//bubble sorting function
-int* bubbleSort(int *array)
-{
-	for(int i=0;i<4;i++)
-	{
-		for(int j=0;j<4-i;j++)
-		{
-			if(array[j] > array[j+1])
+	int i=0,j=0;
+	int temp;
+	
+	for(i=0;i<n-1;i++){
+		
+		for(j=0;j<n-i-1;j++){
+		
+			if(num[j] > num[j+1])
 			{
-				swap(&array[j],&array[j+1]);
+				temp = num[j];
+				num[j] = num[j+1] ;
+				num[j+1] = temp;
+				
 			}
 		}
 	}
 	
-	printf("Sorted array is : ");
-	for(int x=0;x<5;x++)
-	{
-		printf(" %d ",array[x]);
-	}
-	printf("\n");
+	printf("\nSorted Array : \n");
+	for(i=0;i<n;i++){
+		
+		printf("%3d\t",num[i]);	
+	}		
+	
 }
+//function for insertion sort
+void insertion_sort(int num[10], int n) 
+{ 
+    int i, key, j; 
+    for (i = 1; i < n; i++) { 
+        key = num[i]; 
+        j = i - 1; 
+        while (j >= 0 && num[j] > key) { 
+            num[j + 1] = num[j]; 
+            j = j - 1; 
+        } 
+        num[j + 1] = key; 
+    } 
+    
+ 
+	printf("\nSorted Array : \n");
+	for(i = 0;i < n;i++){
+		
+		printf("%3d\t",num[i]);	
+	}		
+	
+}
+ 
+int main(int argc,char *argv[]){
+	//Calling Fork To Create Process	
+	
+	int num[10];
+	int n = 0;	
+	int i = 0;	
+	printf("\nEnter The Number Of Elements: ");
+	scanf("%d",&n);
+	printf("\n");
+	for(i = 0;i < n;i++)
+	{
+		printf("Enter Number %d: ",(i+1));
+		scanf("%d",&num[i]);
+	}	
+	int pid = fork();
+	
+	//Checking if pid =0 then it is chile process
+	if(pid == 0){
+			printf("\nSorting Using Child Process\n");
+			bubble_sort(num,n);		
+	}else{
+			printf("\nParent Is Running...............\n");
+			sleep(5);
+			printf("\nSorting Using Parent Process\n");
+			insertion_sort(num,n);
+			printf("\n");							
+			wait(NULL); //Adding Sleep in parent Process execution
+	}
 
-int main()
-{
-	pid_t cpid;//child process id variable
-	int array[]={5,8,34,9,12};
-	cpid=fork();//fork creates child process
-	//if return status = 0 child is running
-	if(cpid == 0)
-	{
-		printf("child is running \n");
-		printf("Child pid is %d \n", getpid());
-		bubbleSort(array);
-		printf("OLd Parent pid is %d \n", getppid());
-		sleep(1);
-		printf("New Parent pid is %d \n", getppid());
-	}
-	if(cpid > 0)//if return status is greater than zero parent is running
-	{
-		printf("parent is running \n");
-		printf("Parent is %d \n", getpid());
-		bubbleSort(array);
-		sleep(20);
-		wait(NULL);
-	}
-	
-	return 0;
-	
+
+
 }
