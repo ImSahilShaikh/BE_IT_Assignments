f08ed673588e3ea20665ffd5d13e7273e6b67a6d
ImSahilShaikh <43376638+ImSahilShaikh@users.noreply.github.com>
2019-07-29 15:01:09 +0530

    Update and rename sorting2.c to zombie.c

---
+++ TE_IT/SL2/Assignment2/zombie.c
@@ -0,0 +1,64 @@
+//include required libraries
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+//function to swap values
+void swap(int *xp, int *yp)
+{
+	  int temp = *xp; 
+    *xp = *yp; 
+    *yp = temp; 
+}
+
+//bubble sorting function
+int* bubbleSort(int *array)
+{
+	for(int i=0;i<4;i++)
+	{
+		for(int j=0;j<4-i;j++)
+		{
+			if(array[j] > array[j+1])
+			{
+				swap(&array[j],&array[j+1]);
+			}
+		}
+	}
+	
+	printf("Sorted array is : ");
+	for(int x=0;x<5;x++)
+	{
+		printf(" %d ",array[x]);
+	}
+	printf("\n");
+}
+
+int main()
+{
+	pid_t cpid;//child process id variable
+	int array[]={5,8,34,9,12};
+	cpid=fork();//fork creates child process
+	//if return status = 0 child is running
+	if(cpid == 0)
+	{
+		printf("child is running \n");
+		printf("Child pid is %d \n", getpid());
+		bubbleSort(array);
+		printf("OLd Parent pid is %d \n", getppid());
+		sleep(1);
+		printf("New Parent pid is %d \n", getppid());
+	}
+	if(cpid > 0)//if return status is greater than zero parent is running
+	{
+		printf("parent is running \n");
+		printf("Parent is %d \n", getpid());
+		bubbleSort(array);
+		sleep(20);
+		wait(NULL);
+	}
+	
+	return 0;
+	
+}
