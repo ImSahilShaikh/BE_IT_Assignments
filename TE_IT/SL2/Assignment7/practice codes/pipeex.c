894bbc35d0ebe8e96c0d06a87361ec619a167630
ImSahilShaikh <43376638+ImSahilShaikh@users.noreply.github.com>
2019-09-11 13:11:20 +0530

    Update pipeex.c

--- TE_IT/SL2/Assignment7/practice codes/pipeex.c
+++ TE_IT/SL2/Assignment7/practice codes/pipeex.c
@@ -3,25 +3,25 @@
 
 int main()
 {
-  //pipe descriptor
+	//pipe descriptor
 	int f[2];
-  //variable for keeping track of processid
+	//variable for keeping track of processid
 	int pid;
-  //buffer
+	//buffer
 	int readvar[5];
 	
-  //creating pipe
+	//creating pipe
 	pipe(f);
-  //creating new process
+  	//creating new process
 	pid=fork();
 	
-  //if fork fails to create new process
+  	//if fork fails to create new process
 	if(pid<0)
 	{
 		printf("\nERROR: Can't create new process! ERROR in fork!!");
 	}
 	
-  //parent process
+  	//parent process
 	if(pid>0)
 	{
 		printf("\nParent process RUNNING!");
@@ -29,25 +29,25 @@ int main()
 		printf("\nEnter a number: ");
 		scanf("%d",&readvar);
 		
-    //closing reading end of pipe for parent to write
+    	//closing reading end of pipe for parent to write
     close(f[0]);
     
-    //writing entered number to buffer
+    	//writing entered number to buffer
 		write(f[1],readvar,sizeof(readvar));
 		
     //closing pipe
     close(f);
 	}
   
-  //child process
+  	//child process
 	if(pid==0)
 	{
 		printf("\nChild process RUNNING!");
 		
-    //closing writinh end of pipe for child to read
+    	//closing writinh end of pipe for child to read
 		close(f[1]);
     
-    //reading the number from buffer
+    	//reading the number from buffer
 		read(f[0],&readvar,sizeof(readvar));
 		printf("\nEntered number is: %d",readvar[0]);
 		
