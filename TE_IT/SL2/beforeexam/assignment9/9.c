9f748a4055fc6786fa46f41a517f106625e82b86
ImSahilShaikh <43376638+ImSahilShaikh@users.noreply.github.com>
2019-10-19 12:29:21 +0530

    Update 9.c

--- TE_IT/SL2/beforeexam/assignment9/9.c
+++ TE_IT/SL2/beforeexam/assignment9/9.c
@@ -57,10 +57,96 @@ void display()
 	}
 	close(fd);
 }
+void search()
+{
+	int flag=0,key=0;
+	student s;
+	int fd= open("mydb.dat",O_RDONLY);
+	printf("\nEnter a key to be searched");
+	scanf("%d",&key);
+	if(fd<0)
+		printf("ERROR");
+	else
+	{
+		printf("\nROLL NO\tNAME\tMARKS");
+		while(read(fd,&s,sizeof(s)))
+		{
+			if(s.roll == key)
+			{
+				printf("\n%d\t%s\t%d",s.roll,s.name,s.marks);
+				flag=1;
+			}
+		}
+		if(flag==0)
+			printf("\nNo data found");
+	}
+	close(fd);
+}
+void delete(int key)
+{
+	
+	student s;
+	int fd1=open("mydb.dat",O_RDONLY);
+	int fd2=open("temp.dat",O_CREAT|O_WRONLY);
+	
+	if(fd1<0)
+	{printf("\nERROR in reading file");}
+	else
+	{
+		while(read(fd1,&s,sizeof(s)))
+		{
+			if(s.roll==key)
+			{
+				
+			}
+			else
+				write(fd2,&s,sizeof(s));
+		}
+	}
+	remove("mydb.dat");
+	rename("temp.dat","mydb.dat");
+	close(fd1);
+	close(fd2);
+}
+void modify(int key)
+{
+	student s1,s2;
+	int fd1=open("mydb.dat",O_RDONLY);
+	int fd2=open("temp.dat",O_CREAT|O_WRONLY);
+	if(fd1<0)
+	{
+		printf("\nError in reading file");
+	} 	
+	else
+	{
+		while(read(fd1,&s1,sizeof(s1)))
+		{
+			if(s1.roll!=key)
+			{
+				write(fd2,&s1,sizeof(s1));
+			}
+			else
+			{
+				printf("\nEnter new roll no:");
+				scanf("%d",&s2.roll);
 
+				printf("\nEnter new name:");			
+				scanf("%s",&s2.name);
+	
+				printf("\nEnter new marks:");
+				scanf("%d",&s2.marks);
+				write(fd2,&s2,sizeof(s2));
+			}	
+		}
+	}
+	remove("mydb.dat");
+	rename("temp.dat","mydb.dat");
+	close(fd1);
+	close(fd2);
+}
 int main()
 {
-	int ch=0;
+	int ch=0,key=0;
 	while(1)
 	{
 		printf("\n-------------------STUDENT DATABASE-------------------");
@@ -80,13 +166,18 @@ int main()
 				display();
 				break;
 			case 4:				
-				
+				search();
 				break;
 			case 5:
-				//modifyRecord();
+				printf("Enter the roll no you want to modify\n");
+				scanf("%d",&key);
+				modify(key);
 				break;
 			case 6:
 				
+				printf("Enter the roll no you want to delete\n");
+				scanf("%d",&key);
+				delete(key);
 				break;
 			case 7:
 				remove("student_database.txt");
