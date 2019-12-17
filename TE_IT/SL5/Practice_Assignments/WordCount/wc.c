b55ac6a94d04d9d3d830c3570cef0bf1e68ff450
ImSahilShaikh <43376638+ImSahilShaikh@users.noreply.github.com>
2019-12-17 11:27:54 +0530

    Update and rename TE_IT/SL5/Practice_Assignments/wordcount.c to TE_IT/SL5/Practice_Assignments/WordCount/wc.c

--- TE_IT/SL5/Practice_Assignments/WordCount/wc.c
+++ TE_IT/SL5/Practice_Assignments/WordCount/wc.c
@@ -2,11 +2,11 @@
 int main()
 {
 	//declaration of filepointer
-	FILE *fp;
+	FILE *fp,*fop;
 	//buffer variable to store the character and display
 	char c='\0';
 	
-	int word=0,lines=0,characters=0;
+	int word=0,lines=0,characters=0,s;
 	
 	//open file using fopen
 	// use second paramater as w to write if file exist, if not creates new file
@@ -43,5 +43,16 @@ int main()
 
 	printf("\nINFO: File Closed");
 	printf("\n\n*********SUMMARY*********\n1)Characters: %d\n2)Words: %d\n3)Lines: %d\n*************************",characters,word,lines);	
+	
+	fop=fopen("fileinfo.dat","w");
+	if(fop==NULL)
+		printf("\nWARNING: Failed to open the file");
+	else
+		printf("\nINFO: File opened");
+		printf("\nINFO: Writing in the file");
+		s=fprintf(fop,"*********SUMMARY*********\n1)Characters: %d\n2)Words: %d\n3)Lines: %d\n*************************",characters,word,lines);
+		
+		printf("\nINFO: File Closed");
+	fclose(fop);	
 	return 0;
 }
