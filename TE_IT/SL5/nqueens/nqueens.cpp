f2e860be9dfa0eb325ec26cf1864556100eec473
Sahil Shaikh <43376638+ImSahilShaikh@users.noreply.github.com>
2019-12-27 14:25:41 +0530

    Update nqueens.cpp

--- TE_IT/SL5/nqueens/nqueens.cpp
+++ TE_IT/SL5/nqueens/nqueens.cpp
@@ -3,13 +3,22 @@
 #include<stdlib.h>
 using namespace std;
 
-//int chessboard[20][20];
+int array[50];//chessboard
 
-int array[50];
-//where k is no of queens and n is index of chessboard
+void print(int no)
+{
+	cout<<"\n";
+	for(int i=1;i<=no;i++)
+	{	
+		cout<<"\t"<<array[i];
+	}
+}
 
+//k is the number of queen and i is the array index
+//function to check the row column and diagonal conditions
 bool place(int k,int i)
 {
+	//j is column number
 	for(int j=1;j<=k-1;j++)
 	{
 		//check row and diagonal condition
@@ -20,20 +29,22 @@ bool place(int k,int i)
 }
 
 
+//k is number of particular queen and n is total number of queens
+//function to place the queen in the array at proper location
 void nqueens(int k,int n)
 {
+	//check the correct place from first location
 	for(int i=1;i<=n;i++)
 	{
+		//checking the correct place to keep the queen
 		if(place(k,i))
 		{
+			//placing the queen in the array
 			array[k]=i;
 			if(k==n)
 			{
-				cout<<"\n";			
-				for(int i=1;i<=n;i++)
-				{	
-					cout<<"\t"<<array[i];
-				}
+				//printing the output when the last queen is placed
+				print(n);
 			}
 			else
 				nqueens(k+1,n);
@@ -43,17 +54,21 @@ void nqueens(int k,int n)
 
 int main()
 {
-	//size of chessboard
-
 	//no of queens
 	int no=0;
 	
-	cout<<"\nEnter no of queens :";
+	cout<<"\nINFO: Enter no of queens :";
 	cin>>no;
 	
-	cout<<"\nYour chessboard is of size "<<no<<" X "<<no;
+	/*if(no<4)
+	{
+		cout<<"\nWARNING: Please enter atleast 4 queens!!!!";
+		exit(0);
+	}*/
+	cout<<"\nINFO: Your chessboard is of size "<<no<<" X "<<no;
 	
 	//call nqueens function
+	cout<<"\nINFO: All possible solutions for given " <<no<<" X "<<no<< " chessboard are: ";			
 	nqueens(1,no);
 	cout<<endl;
 
