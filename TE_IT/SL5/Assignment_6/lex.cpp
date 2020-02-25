52e76ca8b72272a679a023806a5136a14907184e
Sahil Shaikh <43376638+ImSahilShaikh@users.noreply.github.com>
2020-02-25 12:08:39 +0530

    Update lex.cpp

--- TE_IT/SL5/Assignment_6/lex.cpp
+++ TE_IT/SL5/Assignment_6/lex.cpp
@@ -1,7 +1,41 @@
 #include<iostream>
+#include<stdlib.h>
+
 #include<fstream>
 
 using namespace std;
+
+string key[] = {"auto","break","case","char","const","continue","default","do","double","else","enum","extern","float","for","goto","if","int","long","register","return","short","signed","sizeof","static","struct","switch","typedef","union","unsigned","void","volatile","while"};
+
+int n = sizeof(key)/sizeof(key[0]);
+
+vector <string> keywords (key,key+n);
+
+char special_characters[]={'~','!','#','$','%','^','&','*','(',')','_','+','|','\\','`','=','{','}','[',']',':','"',';','<','>','?',',','.'};
+
+bool checkKeywords(string word)
+{
+	for(int i=0;i<34;i++)
+	{
+		if(word.compare(key[i]))
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+bool checkSpecialCharacters(char x)
+{
+	for(int i=0;i<30;i++)
+	{
+		if(x==special_characters[i])
+		{
+			return true;
+		}
+	}
+	return false;
+}
 
 int main()
 {
@@ -17,10 +51,21 @@ int main()
 		{
 			if(c == '{' || c == '(' || c == ')' || c == '}' || c == '=' || c == '+' || c == ',' || c == ';' || c=='#' || c=='<' || c=='>' || c==' ')
 			{
-				if(c==' ')
-					continue;
-				cout<<word<<endl;
-				cout << c << endl;
+				/*if(c==' ')
+					continue;*/
+				
+					if(checkKeywords(word));
+					{
+						if(word==""|| word ==" ")
+							continue;
+						cout<<word<<": Keyword"<<endl;
+					}
+					// if(checkSpecialCharacters(c))
+					// 	cout<<c<<": Special character"<<endl;
+					// if(!checkKeywords(word) && !checkSpecialCharacters(c))
+					// {
+					// 	cout<<c<<": symbol";
+					// }
 				word.clear();
 			}
 		}
