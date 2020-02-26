8fac6ec3337e6d8b6e8c4e3303d1b0291096c168
Sahil Shaikh <43376638+ImSahilShaikh@users.noreply.github.com>
2020-02-26 14:47:39 +0530

    Update a5.py

--- TE_IT/SL6/PracticeAssignments/a5.py
+++ TE_IT/SL6/PracticeAssignments/a5.py
@@ -1,4 +1,7 @@
+#doing random stuff to learn syntax and functions of python for data science 
+
 import pandas as pd
+import matplotlib.pyplot as plt
 
 df = pd.read_csv("DataSets/dataset_Facebook.csv",sep=';')
 
@@ -8,9 +11,19 @@ print(df)
 
 #slicing data w.r.t print Comments
 print('Summary of Comments\n')
-print(df[0:5]['comment'])
+b=(df[0:5]['comment'])
 
 #slicing data w.r.t print Likes
 
-print('Summary of Likes\n')
-print(df[0:5]['like'])
+#print('Summary of Likes\n')
+a = (df[0:5]['like'])
+plt.plot(a,b,color='red',linestyle='dashed',linewidth=1.1)
+
+#slicing for multiple labels
+print(df.loc[1:6,['Post Month','Paid','Post Weekday']])
+
+plt.xlabel("X-Axis")
+plt.ylabel("Y-Axis")
+plt.title("First Plot in Python")
+plt.legend()
+plt.show()
