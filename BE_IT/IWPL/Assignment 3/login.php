311298b8681bcc0706cdf0969d723178378d0e39
ImSahilShaikh <imsas.shaikh3@gmail.com>
2021-02-14 23:32:27 +0530

    php login register done

--- BE_IT/IWPL/Assignment 3/login.php
+++ BE_IT/IWPL/Assignment 3/login.php
@@ -1,8 +1,13 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="error.css">
     <title>EPIC REVIEW</title>
 </head>
 
@@ -25,23 +30,37 @@
     <div class="about-container">
         <div class="about-card">
             <div class="about-body">
+
                 <div class="about-title">
                     <strong class="title-text" style="margin: 20px 0px;"> Login</strong>
-                    <form action="">
-                    <div class="formrow">
-                        <input type="text" name="username" id="username" placeholder="Username"> 
-                    </div>
-                    <div class="formrow">
-                        <input type="password" name="password" id="password" placeholder="Password">
-                    </div>
-                    <div class="formrow">
-
-                    </div>
-                </form>
+                    <form action="login-action.php" method="POST">
+                        <div class="formrow">
+                            <input type="text" name="username" id="username" placeholder="Username">
+                        </div>
+                        <div class="formrow">
+                            <input type="password" name="password" id="password" placeholder="Password">
+                        </div>
+                        <div class="formrow" style="margin-bottom: 20px;">
+                            <input type="submit" id="login" name="login">
+                        </div>
+                        <div class="formrow" style="margin-bottom: 20px;">
+                            <?php
+                            if (isset($_SESSION["error"])) {
+                                $error = $_SESSION["error"];
+                                echo "<span class='error'>$error</span>";
+                            }
+                            ?>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
+
 </body>
 
 </html>
+
+<?php
+unset($_SESSION["error"]);
+?>
