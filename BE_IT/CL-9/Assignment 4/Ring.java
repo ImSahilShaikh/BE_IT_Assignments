48928be7808516de7db7e5e941e50f4684a4cdd3
ImSahilShaikh <imsas.shaikh3@gmail.com>
2021-05-17 23:46:21 +0530

    Update Ring.java

--- BE_IT/CL-9/Assignment 4/Ring.java
+++ BE_IT/CL-9/Assignment 4/Ring.java
@@ -2,14 +2,18 @@ import java.util.*;
 
 public class Ring{
 
-    ArrayList<Integer> processId;
-    boolean processes[];
-    int coordinator;
-    int no_process;
-    int no;
+    // Variable declarations
+    ArrayList<Integer> processId; //arraylist to store the list of process ids
+    boolean processes[]; // processes
+    int coordinator; //coordinator process
+    int no_process; //total number of processes to be created
+    int no; // the number of a process
 
+    //Constructor
     public Ring(int no_process)
     {
+        // Initializing all the variables
+
         processId = new ArrayList<Integer>();
         coordinator = no_process;
         processes = new boolean[no_process];
@@ -18,13 +22,14 @@ public class Ring{
         for(int i=0;i<no_process;i++)
         {
             System.out.println("Process P["+(i+1)+"] created");
-            processes[i] = true;
+            processes[i] = true; //default value of newly created process is true so that it will be active
         }
         
         this.no_process = no_process;
         System.out.println("Coordinator Process: "+ this.coordinator);
     }
 
+    //Method to display all active processes
     void showActiveProcesses()
     {
         System.out.println("The active processes are: ");
@@ -40,11 +45,14 @@ public class Ring{
         }
     }
 
+    //Method to take down a process i.e make that process inactive
     void downProcess(int no){
+        //if already down
         if(!processes[no-1])
         {
             System.out.println("Process P["+no+"] is already down");
         }
+        //else set process no inactive
         else
         {
             processes[no-1] = false;
@@ -52,18 +60,22 @@ public class Ring{
         }
     }
 
+    //Method to show the current coordinator process
     void showCurrentCoordinator()
     {
         System.out.println("Process P["+coordinator+"] is the current Coordinator");
     }
 
+    //Method to initiate the election using a process number
     void startElection(int no)
     {
+        
         if(processes[no-1])
         {
+            //if the initiator process is active add that processid into arraylist
             processId.add(no);
+            
             int i = no;
-
             System.out.print("Process P["+no+"] sending following list: [");
 
             for(Integer n : processId)
@@ -72,9 +84,12 @@ public class Ring{
             }
             System.out.print("]");
             System.out.println("");
+
+            //iterate and add all the process ids till the ring is traversed
             while(i != no-1)
             {
                 if(processes[i]){
+                    //add the active process in the arraylist
                     processId.add(i+1);
                     System.out.print("Process P["+no+"] sending following list: [");
 
@@ -90,6 +105,8 @@ public class Ring{
                 
                 i = (i+1) % no_process;
             }
+            
+            // set new coordinator as the max no of process id from the arralist
             int new_coordinator = Collections.max(processId);
             System.out.println("Process P["+no+"] declares P["+new_coordinator+"] as new Coordinator");
             coordinator = new_coordinator;
